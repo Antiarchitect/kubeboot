0262a2e0dff88472602a78ccba8f84778b472dd1
Andrey Voronkov <voronkovaa@gmail.com>
2017-11-27 12:16:28 +0300

    Helm init should be a part of platform/framework -dependent library?

--- lib/up.sh
+++ lib/up.sh
@@ -5,5 +5,5 @@ minikube_status=$(minikube status --format {{.MinikubeStatus}})
 if [[ ${minikube_status} != "Running" ]]; then
   minikube start
 fi
-helm init --upgrade --kube-context "${context_name}"
+helm init --upgrade --kube-context "${context_name}" # Not sure if it belongs here. Should it be placed into language library part?
 kubectl config use-context "${context_name}" # Ensure we are working with minikube context.
