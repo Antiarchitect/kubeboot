3ba6a2a6481337453931da7ca8c5606aa4139509
Andrey Voronkov <voronkovaa@gmail.com>
2017-11-30 00:46:18 +0300

    Changel local docker registry setup in favor of Helm.

--- lib/up.sh
+++ lib/up.sh
@@ -9,5 +9,13 @@ if [[ ${minikube_status} != "Running" ]]; then
 fi
 eval $(minikube docker-env) # Need to explore this more thoroughly.
 helm init --upgrade --kube-context "${context_name}" # Not sure if it belongs here. Should it be placed into language library part?
+helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator # Adding incubator repository.
 kubectl config use-context "${context_name}" # Ensure we are working with minikube context.
-kubectl apply -f ./k8s-templates/local-registry.yml
+
+if [[ $(helm list --all --short | fgrep local-docker-registry | wc -l) == 0 ]]; then
+  helm install incubator/docker-registry --name local-docker-registry
+fi
+
+if [[ $(helm list --all --short | fgrep local-prometheus | wc -l) == 0 ]]; then
+  helm install stable/prometheus --name local-prometheus
+fi
