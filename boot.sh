5032404408b3e3117b0d757f730f8b214a9e4dc3
Andrey Voronkov <voronkovaa@gmail.com>
2017-12-02 00:03:55 +0300

    ASDF installation added.

--- boot.sh
+++ boot.sh
@@ -7,7 +7,7 @@ set -o pipefail
 . ./lib/check.sh
 fun_run_check
 
-if [[ "${#PROPOSE_INSTALL[@]}" -gt 0 && -z "${PROPOSE_INSTALL[ASDF]+x}" ]]; then
+if [[ "${#PROPOSE_INSTALL[@]}" -gt 0 ]]; then
   . ./lib/install.sh
   fun_run_check
 fi
--- lib/install.sh
+++ lib/install.sh
@@ -23,15 +23,34 @@ fun_asdf_install() {
   asdf global "${plugin_name}" "${plugin_version}"
 }
 
-if [[ "${PROPOSE_INSTALL[Helm]}" = true ]]; then
+if [[ "${PROPOSE_INSTALL[ASDF]}" == true ]]; then
+  rm -rf $HOME/.asdf
+  git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.4.0
+
+
+  filepath="$HOME/.bashrc"
+
+  if [[ $(cat ${filepath} | fgrep '. $HOME/.asdf/asdf.sh' | wc -l) == 0 ]]; then
+    echo -e '\n. $HOME/.asdf/asdf.sh' >> ${filepath}
+  fi
+  chmod +x "$HOME/.asdf/asdf.sh"
+  . "$HOME/.asdf/asdf.sh"
+
+  if [[ $(cat ${filepath} | fgrep '. $HOME/.asdf/completions/asdf.bash' | wc -l) == 0 ]]; then
+    echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ${filepath}
+  fi
+  chmod +x "$HOME/.asdf/completions/asdf.bash"
+fi
+
+if [[ "${PROPOSE_INSTALL[Helm]}" == true ]]; then
   fun_asdf_install helm "${HELM_VERSION}" https://github.com/Antiarchitect/asdf-helm.git
 fi
 
-if [[ "${PROPOSE_INSTALL[MiniKube]}" = true ]]; then
+if [[ "${PROPOSE_INSTALL[MiniKube]}" == true ]]; then
   fun_asdf_install minikube "${MINIKUBE_VERSION}"
 fi
 
-if [[ "${PROPOSE_INSTALL[kubectl]}" = true ]]; then
+if [[ "${PROPOSE_INSTALL[kubectl]}" == true ]]; then
   fun_asdf_install kubectl "${KUBECTL_VERSION}"
 fi
 
