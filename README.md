944f9aeea5da0badc3565e32bf18c554179b80eb
Andrey Voronkov <voronkovaa@gmail.com>
2017-12-15 06:58:47 +0300

    * Remove extra helm releases from initial deploy. * Allow root access to minikube machine. * Seed Minikube machine with unison binaries.

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 * Check if installed version of the component is lower than required. Version upgrade abiltiy.
 * Cleanup. Remove all installable components along with config files.
 
-# Notes
-Minikube config is at $HOME/.minikube
+# Up
+## Minikube part
+```bash
+minikube delete || true && ${HOME}/PROJECTS/kubeboot/kb.sh && eval $(minikube docker-env) && docker build ${HOME}/PROJECTS/docker-rails/ --tag my-rails-dev --build-arg uid=${UID} && helm install --name my-rails-dev ../helm-rails/
+```
+
+## Unison part
+```bash
+~/unison ${HOME}/PROJECTS/testapp-postgresql ssh://root@$(minikube ip)//app -sshargs "-o StrictHostKeyChecking=no -i $(minikube ssh-key)" -ignorearchives -owner -group -numericids -auto -batch -repeat watch -ignore "Path tmp/pids"
+```
--- lib/up.sh
+++ lib/up.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+echo -e "${G}Copying unison sync tool binaries...${NONE}"
+minikube_bindir_seed="${HOME}/.minikube/files/usr/bin"
+mkdir -p $minikube_bindir_seed
+cp ${BASEDIR}/bin/unison ${BASEDIR}/bin/unison-fsmonitor $minikube_bindir_seed
+
 context_name="minikube"
 set +e
 minikube_status=$(minikube status --format {{.MinikubeStatus}})
 set -e
 if [[ ${minikube_status} != "Running" ]]; then
   echo -e "${G}Starting Minikube...${NONE}"
-  minikube start --insecure-registry localhost:5000
-#  su -c "CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --insecure-registry localhost:5000"
+  minikube start
 fi
-eval $(minikube docker-env) # Need to explore this more thoroughly. VM drivers only.
+
+echo -e "${G}Allowing root access to the Minikube node...${NONE}"
+minikube ssh "sudo mkdir -p /root/.ssh"
+minikube ssh "sudo chmod 700 /root/.ssh"
+minikube ssh "sudo cp /home/docker/.ssh/authorized_keys /root/.ssh"
+minikube ssh "sudo chown -R root:root /root/.ssh"
+minikube ssh "sudo mkdir -p /app"
+
 echo -e "${G}Initializing Helm...${NONE}"
 helm init --upgrade --kube-context "${context_name}" # Not sure if it belongs here. Should it be placed into language library part?
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator # Adding incubator repository.
@@ -22,20 +33,10 @@ for i in {1..150}; do # timeout for 5 minutes
   if [ $? -eq 0 ]; then
     break
   fi
-  sleep 2
+  sleep 1
 done
 
 # Tiller wait workaround https://github.com/kubernetes/helm/issues/2114
 # We should wait until --wait option will be available for `helm init`
 echo -e "${G}Rolling out Tiller deployment...${NONE}"
 kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system;
-
-if [[ $(helm list --all --short | fgrep local-docker-registry | wc -l) == 0 ]]; then
-  echo -e "${G}Installing local docker registry....${NONE}"
-  helm install incubator/docker-registry --name local-docker-registry
-fi
-
-# Prometheus is too heavy for now.
-#if [[ $(helm list --all --short | fgrep local-prometheus | wc -l) == 0 ]]; then
-#  helm install stable/prometheus --name local-prometheus
-#fi
